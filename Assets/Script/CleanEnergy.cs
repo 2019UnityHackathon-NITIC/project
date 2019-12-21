73f88102dd45088d3ac73b4a1b51aef851f965b8
isso0424 <isso04240129@gmail.com>
2019-12-22 01:25:59 +0700

    疲れた

--- Assets/Script/CleanEnergy.cs
+++ Assets/Script/CleanEnergy.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 public class CleanEnergy : MonoBehaviour
 {
     [SerializeField]private int _pointBigger = 5;
+    [SerializeField]private AudioClip _SE;
+    [SerializeField]private AudioSource _audioSource;
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        _audioSource.PlayOneShot(_SE);
         if (!other.gameObject.CompareTag("Player")) return;
         Parameters.CleanEnergy += _pointBigger;
         Destroy(gameObject);
